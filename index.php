d3b9838e91c69b133de99017cb2b52ed8894a80c
nick-knoops <nick@wearewim.nl>
2020-09-10 09:18:53 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <!--[if lt IE 9]>
+        <script src="js/shiv/html5shiv.min.js"></script>
+        <script src="js/shiv/html5shiv-printshiv.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="product-configurator-wrap">
+        <div class="slide">
+            <div class="half">
+                <h2 class="step-title">Stap 1: Type kap</h2>
+
+            </div>
+            <div class="half">
+            
+            </div>
+        </div>    
+    </div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="js/vendor/modernizr-custom.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
